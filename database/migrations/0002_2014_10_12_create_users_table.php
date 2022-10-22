b3ac5cd4392f3a34c64ae4bd84bd7022c9e49971
FrancoisCAN <francois.cancalon@gmail.com>
2022-10-22 16:24:34 +0200

    feat(auth): ajout des migrations

--- database/migrations/0002_2014_10_12_create_users_table.php
+++ database/migrations/0002_2014_10_12_create_users_table.php
@@ -15,12 +15,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('city');
+            $table->tinyText('country');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('password');
+            $table->string('phone');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
---
+++ database/migrations/0006_2022_10_22_create_devices_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->tinyText('country');
+            $table->ipAddress('ip');
+            $table->boolean('is_mobile');
+            $table->string('name');
+            $table->string('organization');
+            $table->string('region');
+            $table->string('zip');
+            $table->timestamps();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+};
