7f0652875a802a43b49bea43a04f9c77a6abe100
FrancoisCAN <francois.cancalon@gmail.com>
2022-10-26 01:16:08 +0200

    feat(auth): ajout des appareils liés à l'utilisateur

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,9 +12,13 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
-        //
+        /**
+         * Ignore Sanctum migrations as it has been moved
+         * to the current migrations' folder.
+         */
+        Sanctum::ignoreMigrations();
     }
 
     /**
@@ -21,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         //
     }
